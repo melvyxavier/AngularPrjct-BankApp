7be441becabf7cafb88237a22e246b1f8f6ac602
Melvy Xavier <melvyxavier7@gmail.com>
2023-09-23 23:01:05 +0530

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AccStatementComponent } from './acc-statement/acc-statement.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
-  {path:"",component:LoginComponent},
-  {path:"signup",component:SignupComponent},
-  {path:"home",component:HomepageComponent},
-  {path:"history",component:AccStatementComponent}
+  { path: "", component: HeaderComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "home", component: HomepageComponent },
+  { path: "history", component: AccStatementComponent }
 ];
 
 @NgModule({
